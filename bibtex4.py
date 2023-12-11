8283c23e862ce72213e6425ebf7cf3712fec34bd
Coder731 <stephenobrien1@gmail.com>
2023-12-11 18:27:02 +0000

    bibtex4 py Previous attempt before main3 py
    in last commit
    
    Attempt to
    loop on
    known
    iterator
    
    See main4 py for
    improved attempt

--- bibtex4.py
+++ bibtex4.py
@@ -33,9 +33,12 @@ for line in file:
     if (re.search('^journal',line)):
         tokens = nltk.word_tokenize(line)
         tagged = nltk.pos_tag(tokens)
-        remove_end = tagged[0:-2]
+        remove_end = tagged[0:-2] # cut space and last curly bracket
         for line in remove_end:
-            holder+=line[0]+' '
+            if(x[position]!=length(whatever_passed_in_string)):
+                holder+=line[0]+' '
+            else:
+                holder+=line[0]
             if(re.search('^journal',holder)):
                 start_after_journal=slice(len_journal,None,1)
                 no_journal=holder[start_after_journal]
