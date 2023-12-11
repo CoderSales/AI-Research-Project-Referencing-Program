9bc98f9ce0216b847ebd922362ea30a930615ac5
Coder731 <stephenobrien1@gmail.com>
2023-12-11 08:41:51 +0000

    bibtex5 py Copy across parts to do with attempt to solve import bug

---
+++ bibtex5.py
@@ -0,0 +1,34 @@
+# import numpy
+# import pandas
+# import openpyxl
+from openpyxl import Workbook
+import datetime
+import os
+import shutil
+import os.path
+
+# import parser1
+
+import re
+
+import importlib
+
+# importlib.reload(parser)
+
+# del parser
+
+# import argparse
+# import sys
+# os_path_basename = os.path.basename(sys.argv[0])
+# print(os_path_basename)
+# argparse.ArgumentParser()
+
+from importlib.metadata import version  
+
+print(importlib.metadata)
+
+# import parser
+# importlib.reload(parser)
+
+
+# parser1.parse()
