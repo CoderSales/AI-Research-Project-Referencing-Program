7fcbf682f977c5d090abc4810ab7806ac1946ef8
Coder731 <stephenobrien1@gmail.com>
2023-12-11 13:59:48 +0000

    Get length of word journal

--- bibtex4.py
+++ bibtex4.py
@@ -19,7 +19,7 @@ your_path = os.environ.get("PWD")
 root = your_path
 files = [(path,f) for path,_,file_list in os.walk(root) for f in file_list]
 f_name="Bibtex.bib"
-from shutil import  copy
+from shutil import copy
 # f = open(os.path.join(your_path, f_name), 'r', encoding="utf8")
 # for i in f.readlines():
 #     print(i)
@@ -27,11 +27,15 @@ from shutil import  copy
 
 file = open("Bibtex.bib", 'r', encoding="utf8")
 
+holder=""
+len_journal=len('journal')
+# print(len_journal)
 for line in file:
     if (re.search('^journal',line)):
         tokens = nltk.word_tokenize(line)
         tagged = nltk.pos_tag(tokens)
         remove_end = tagged[0:-1]
         for line in remove_end:
-            print(line[0])
-            line[0]
+            # print(line[0])
+            holder+=line[0]
+print(holder)
