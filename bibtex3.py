56842a9e5f8193ceea514a47a728000f3390226b
Coder731 <stephenobrien1@gmail.com>
2023-12-11 00:42:34 +0000

    Update bibtex3 py

--- bibtex3.py
+++ bibtex3.py
@@ -1,31 +1,43 @@
-import numpy
-import pandas
-import openpyxl
+# import numpy
+# import pandas
+# import openpyxl
 from openpyxl import Workbook
-wb = Workbook()
-ws = wb.active
 import datetime
 import os
-
-print(os.environ.get("PWD"))
-
-
-your_path = os.environ.get("PWD")
-
-root = your_path
-
-files = [(path,f) for path,_,file_list in os.walk(root) for f in file_list]
-
 import shutil
 import os.path
+wb = Workbook()
+ws = wb.active
+treeData = [["Type", "Leaf Color", "Height"], ["Maple", "Red", 549], ["Oak", "Green", 783], ["Pine", "Green", 1204]]
+for row in treeData:
+    ws.append(row)
 
-print(your_path)
-
+your_path = os.environ.get("PWD")
+root = your_path
+files = [(path,f) for path,_,file_list in os.walk(root) for f in file_list]
 f_name="Bibtex.bib"
 from shutil import  copy
-
 f = open(os.path.join(your_path, f_name), 'r', encoding="utf8")
 for i in f.readlines():
     print(i)
-
 f.close()
+
+
+from openpyxl import Workbook
+wb = Workbook()
+
+# grab the active worksheet
+ws = wb.active
+
+# Data can be assigned directly to cells
+ws['A1'] = 42
+
+# Rows can also be appended
+ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+import datetime
+ws['A2'] = datetime.datetime.now()
+
+# Save the file
+wb.save("sample.xlsx")
