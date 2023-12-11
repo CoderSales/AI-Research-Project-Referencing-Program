48132ebd197fdcd27f1bce1d2a0dfce6aaef24b4
Coder731 <stephenobrien1@gmail.com>
2023-12-11 23:38:03 +0000

    cut curly bracket from start of journal value
    Add to descriptor
    
    in print statement
    
    on line 38
    
    bibtex py
    
    TODO
    - add spaces
    
    Bug
    
    for if combination
    - from lines
      57 to 71
      seem like
      they would
      add a space
      between the
      each of the
      elements
      assembled
    
      - However
         each element
         seems to
         have been
         concatenated
         into holder2
         with
         no space
         between the elements
    
      - this results in
         holder2 element 0
         being a string without
         spaces

--- bibtex4.py
+++ bibtex4.py
@@ -35,7 +35,7 @@ for line in file:
         tagged = nltk.pos_tag(tokens)
         remove_end = tagged[0:-2] # cut space and last curly bracket
         for k,line in enumerate(remove_end, start = 0):
-            print("line[0] :", line[0])
+            print("on line 38 is: line[0] :", line[0])
             # print("line[0] :", line[0])
             if(line[0]!=len(remove_end)): # remove_end may be line instead
                 holder+=line[0]+' '
@@ -45,5 +45,32 @@ for line in file:
                 start_after_journal=slice(len_journal,None,1)
                 no_journal=holder[start_after_journal]
 
+no_journal
 
-print(no_journal)
+tokens_no_journal = nltk.word_tokenize(no_journal)
+tagged_tokens_no_journal = nltk.pos_tag(tokens_no_journal)
+no_journal_minus_starting_curly_bracket = tagged_tokens_no_journal[1:] # cut space and last curly bracket
+
+holder2=''
+print("line 55 (pre last for-loop-if-else-combination) : no_journal_minus_starting_curly_bracket :", no_journal_minus_starting_curly_bracket)
+
+for k2,v2 in enumerate(no_journal_minus_starting_curly_bracket):
+    # if (v2[0]):
+    if (k2==len(no_journal_minus_starting_curly_bracket)):
+        # temporary test:
+        typev2 = type(v2)
+        print('typev2 =', typev2)
+        print('v2 =', v2)
+        print('v2[0] :',v2[0])
+        typev2at0=type(v2[0])
+        print('typev2at0 :',typev2at0)
+        # add space if there is another word
+        holder2+=v2[0]+ ' '
+    else:
+        # no space if last element
+        holder2+=v2[0]
+
+print('line 73 (post last for-loop-if-else-combination, which assembles holder2) holder2: ', holder2 )
+
+# print('line 75 (post last for-loop-if-else-combination, which assembles holder2) holder2[0]: ', holder2[0] )
+
