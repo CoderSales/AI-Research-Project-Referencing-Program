dc37a3d8aed12371113ba91de356308e8ea17cbf
Coder731 <stephenobrien1@gmail.com>
2023-12-12 20:54:08 +0000

    Add lines from journal py to author py

---
+++ lines/author.py
@@ -0,0 +1,29 @@
+# imports
+
+import nltk
+import re
+
+# file
+
+f_name="Bibtex.bib"
+file = open("Bibtex.bib", 'r', encoding="utf8")
+
+# end prep
+
+lines=1
+file1=file
+for line1 in file1:
+    lines=lines+1
+    
+print(lines, "lines in BibTex.bib")
+
+endline=''
+file = open("Bibtex.bib", 'r', encoding="utf8")
+for line in file:
+    if (re.search('^journal',line)):
+        starter=line
+        line=line.lstrip('journal = {')
+        line=line.rstrip('},\n')
+
+        endline=line
+print(endline)
