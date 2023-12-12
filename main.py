61e02bbab6f11220b6df2396c5b039b1e6f75b44
Coder731 <stephenobrien1@gmail.com>
2023-12-12 21:39:49 +0000

    add import to main py
    from
    
    lines folder
    
    add
    
    init py
    files
    
    to lines folder
    
    and
    
    root directroy
    latter may be
    unnecessary

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+This file calls:
+- author
+- journal
+
+function inside
+file
+for each
+"""
+
+# from dirFoo.dirFoo1.foo1 import Foo1
+
+import lines
+
+from lines import journal
+
+journal.journal()
+
+# from .lines import journal
+
+# from .lines.journal import journal
