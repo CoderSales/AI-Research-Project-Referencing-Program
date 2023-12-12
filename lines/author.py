5a8bc2767c6815931a0bf3e78c0e9fc9e2241704
Coder731 <stephenobrien1@gmail.com>
2023-12-12 21:11:39 +0000

    encapsulate author program in a function
    called author

--- lines/author.py
+++ lines/author.py
@@ -1,34 +1,39 @@
-# imports
+def author():
+    """
+    encapsulate author program in a
+    function
+    """
+    # imports
 
-import nltk
-import re
+    import nltk
+    import re
 
-# file
+    # file
 
-f_name="BibTex.bib"
+    f_name="BibTex.bib"
 
-def opener(f_name):
-    file = open(f_name, 'r', encoding="utf8")
-    return file
+    def opener(f_name):
+        file = open(f_name, 'r', encoding="utf8")
+        return file
 
-file=opener(f_name)
+    file=opener(f_name)
 
-# end prep
+    # end prep
 
-lines=1
-file1=file
-for line1 in file1:
-    lines=lines+1
-    
-print(lines, "lines in BibTex.bib")
+    lines=1
+    file1=file
+    for line1 in file1:
+        lines=lines+1
+        
+    print(lines, "lines in BibTex.bib")
 
-endline=''
-file = opener(f_name)
-for line in file:
-    if (re.search('^journal',line)):
-        starter=line
-        line=line.lstrip('journal = {')
-        line=line.rstrip('},\n')
+    endline=''
+    file = opener(f_name)
+    for line in file:
+        if (re.search('^journal',line)):
+            starter=line
+            line=line.lstrip('journal = {')
+            line=line.rstrip('},\n')
 
-        endline=line
-print(endline)
+            endline=line
+    print(endline)
