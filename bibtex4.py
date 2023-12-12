55fc9136cfa745a7c7dd07e8f4f0426b15644869
Coder731 <stephenobrien1@gmail.com>
2023-12-12 00:08:00 +0000

    Update print statement line number in descriptor2

--- bibtex4.py
+++ bibtex4.py
@@ -60,8 +60,8 @@ for k2,v2 in enumerate(no_journal_minus_starting_curly_bracket):
         print('length of this loop is:', len(no_journal_minus_starting_curly_bracket))
         if (k2+1!=len(no_journal_minus_starting_curly_bracket)):
 
-            print('line 65 is : no_journal_minus_starting_curly_bracket[k2] :', no_journal_minus_starting_curly_bracket[k2])
-            print('line 66 is : no_journal_minus_starting_curly_bracket[k2+1] :', no_journal_minus_starting_curly_bracket[k2+1])
+            print('line 63 is : no_journal_minus_starting_curly_bracket[k2] :', no_journal_minus_starting_curly_bracket[k2])
+            print('line 64 is : no_journal_minus_starting_curly_bracket[k2+1] :', no_journal_minus_starting_curly_bracket[k2+1])
         
         # and, if it's not a comma
         if(v2[0]==','):
