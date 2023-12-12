9bc41017099ce063240b00a2d17eb21b54d75031
Coder731 <stephenobrien1@gmail.com>
2023-12-12 00:06:51 +0000

    minor edit Cut line break

--- bibtex4.py
+++ bibtex4.py
@@ -63,7 +63,6 @@ for k2,v2 in enumerate(no_journal_minus_starting_curly_bracket):
             print('line 65 is : no_journal_minus_starting_curly_bracket[k2] :', no_journal_minus_starting_curly_bracket[k2])
             print('line 66 is : no_journal_minus_starting_curly_bracket[k2+1] :', no_journal_minus_starting_curly_bracket[k2+1])
         
-        
         # and, if it's not a comma
         if(v2[0]==','):
             # add space if there is another word
