5be4e094fc9a08b4d149affbda4a0cbc0a7755c9
CoderSales <CoderSales@users.noreply.github.com>
2026-09-05 16:36:13 +0100

    Refactor main3.py
     to improve journal extraction logic
    
     and ensure nltk punkt is downloaded

--- legacy_file_holder/01legacy_files/main3.py
+++ legacy_file_holder/01legacy_files/main3.py
@@ -1,4 +1,6 @@
-import nltk
+import nltk 
+nltk.download('punkt')
+
 import re
 
 file = open("Bibtex.bib", 'r', encoding="utf8")
@@ -6,18 +8,22 @@ file = open("Bibtex.bib", 'r', encoding="utf8")
 holder=""
 
 line='journal={'
+no_journal=None
 
 if (re.search('^journal',line)):
     tokens = nltk.word_tokenize(line)
     tagged = nltk.pos_tag(tokens)
     remove_end = tagged[0:-2] # cut space and last curly bracket
-    for line in remove_end:
-        if(x[position]!=length(whatever_passed_in_string)):
+    for k, line in enumerate(remove_end, start=0):
+        if(line[k]!=len(remove_end)):
             holder+=line[0]+' '
         else:
             holder+=line[0]
         if(re.search('^journal',holder)):
-            start_after_journal=slice(len_journal,None,1)
+            start_after_journal=slice(line,None,1)
             no_journal=holder[start_after_journal]
 
-print(no_journal)
+# uncomment
+# only works if no_journal exists:
+if no_journal!=None:
+    print(no_journal)
